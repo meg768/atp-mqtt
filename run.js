f6ba938ad26694479c5799d890750622c97748da
meg768 <magnus@egelberg.se>
2026-06-25 16:04:18 +0200

    Move serve marker after player name

--- run.js
+++ run.js
@@ -301,10 +301,10 @@ function createShortLabel(playerA, playerB) {
 function createScoreboardLabel(match) {
   const playerALastName = getLastName(match.playerA?.name);
   const playerBLastName = getLastName(match.playerB?.name);
-  const playerAPrefix = match.serve === "player" ? "•" : "";
-  const playerBPrefix = match.serve === "opponent" ? "•" : "";
+  const playerASuffix = match.serve === "player" ? "•" : "";
+  const playerBSuffix = match.serve === "opponent" ? "•" : "";
 
-  return `${playerAPrefix}${playerALastName} vs ${playerBPrefix}${playerBLastName}`;
+  return `${playerALastName}${playerASuffix} vs ${playerBLastName}${playerBSuffix}`;
 }
 
 function formatOdds(value) {
